e37316e27af9e867057df0bf08ceda10c5c7fa97
munsra <munsra@gmail.com>
2024-02-04 10:40:22 +0100

    Translated comments

--- src/app/thermostat/thermostat.component.ts
+++ src/app/thermostat/thermostat.component.ts
@@ -6,12 +6,12 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './thermostat.component.css'
 })
 export class ThermostatComponent implements OnInit{
-  @Input() min: number = 10;
-  @Input() max: number = 40;
-  @Input() value: number = 15; // Valore iniziale dello slider
+  @Input() min: number = 0;
+  @Input() max: number = 50;
+  @Input() value: number = 10; // Default thermostat value
 
   get targetAngle() {
-    // Converte il valore dello slider in un angolo
+    // Converts the value into angle
     return 225 + (this.value / this.max) * 270;
   }
 
